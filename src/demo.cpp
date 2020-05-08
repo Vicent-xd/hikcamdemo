885f1edfed664a4f1487fe9e8c15a0ab7e651025
vicent <vicent@ubuntu.lan>
2020-05-08 22:25:48 +0800

    realplay ok ptzcontrol ok

--- src/demo.cpp
+++ src/demo.cpp
@@ -9,6 +9,9 @@
 //#include "plaympeg4.h"
 #include "../include/PlayM4.h"
 #include "../include/LinuxPlayM4.h"
+
+//#include <QDir>
+#include <fstream>
  
 #define HPR_ERROR       -1
 #define HPR_OK           0
@@ -18,7 +21,8 @@ static cv::Mat dst;
 
 HWND h = NULL;
 LONG nPort=-1;
-LONG lUserID;
+//LONG lUserID;
+long lRealPlayHandle;
 
 pthread_mutex_t mutex;
 std::list<cv::Mat> g_frameList;
@@ -29,7 +33,7 @@ using namespace cv;
 void CALLBACK DecCBFun(LONG nPort, char *pBuf, LONG nSize, FRAME_INFO *pFrameInfo, void* nReserved1, LONG nReserved2)
 {
    long lFrameType = pFrameInfo->nType;
- 
+     //std::cerr << lFrameType << std::endl; 
      if (lFrameType == T_YV12)
      {
       //cv::Mat dst(pFrameInfo->nHeight, pFrameInfo->nWidth,
@@ -41,6 +45,8 @@ void CALLBACK DecCBFun(LONG nPort, char *pBuf, LONG nSize, FRAME_INFO *pFrameInf
            cv::cvtColor(src, dst, cv::COLOR_YUV2BGR_YV12);//CV_YUV2BGR_YV12);
            //pthread_mutex_lock(&mutex);
            g_frameList.push_back(dst);
+	   cv::imshow("bgr", dst);
+           cv::waitKey(10);
           // pthread_mutex_unlock(&mutex);
      }
     usleep(1000);
@@ -71,7 +77,7 @@ void CALLBACK g_RealDataCallBack_V30(LONG lRealHandle, DWORD dwDataType, BYTE *p
    {
        BOOL inData = PlayM4_InputData(nPort, pBuffer, dwBufSize);
    }*/
-   std::cerr << "PlayM4 \n" << std::endl;
+   //std::cerr << dwDataType << std::endl;
    DWORD dRet;
    switch (dwDataType)
    {
@@ -110,10 +116,10 @@ void CALLBACK g_RealDataCallBack_V30(LONG lRealHandle, DWORD dwDataType, BYTE *p
          }
  
          //打开音频解码, 需要码流是复合流
-         if (!PlayM4_PlaySound(nPort)) {
+         /*if (!PlayM4_PlaySound(nPort)) {
            dRet = PlayM4_GetLastError(nPort);
            break;
-         }
+         }*/
        }
        break;
        //usleep(500);
@@ -148,33 +154,15 @@ void CALLBACK g_ExceptionCallBack(DWORD dwType, LONG lUserID, LONG lHandle, void
     }
 }
 
-int main()
-{
-    NET_DVR_Init();
-    NET_DVR_SetExceptionCallBack_V30(0, NULL, g_ExceptionCallBack, NULL);
-    long lUserID;
-    //login
-    NET_DVR_USER_LOGIN_INFO struLoginInfo = {0};
-    NET_DVR_DEVICEINFO_V40 struDeviceInfoV40 = {0};
-    struLoginInfo.bUseAsynLogin = false;
-
-    struLoginInfo.wPort = 8000;
-    memcpy(struLoginInfo.sDeviceAddress, "172.17.1.99", NET_DVR_DEV_ADDRESS_MAX_LEN);
-    memcpy(struLoginInfo.sUserName, "admin", NAME_LEN);
-    memcpy(struLoginInfo.sPassword, "Ab12345678", NAME_LEN);
-    lUserID = NET_DVR_Login_V40(&struLoginInfo, &struDeviceInfoV40);
-
-    if (lUserID < 0)
-    {
-        printf("pyd1---Login error, %d\n", NET_DVR_GetLastError());
-        return -1;
-    }
+int Demo_RealPlay(LONG lUserID)
+{         
     //Set callback function of getting stream.
-    long lRealPlayHandle;
+    //long lRealPlayHandle;
+    /*
     NET_DVR_PREVIEWINFO struPlayInfo = {0};
     struPlayInfo.lChannel     = 1;  //channel NO
     struPlayInfo.dwLinkMode   = 0;
-    struPlayInfo.bBlocked = 1;
+    struPlayInfo.bBlocked = 0;
     struPlayInfo.dwDisplayBufNum = 1;
     lRealPlayHandle = NET_DVR_RealPlay_V40(lUserID, &struPlayInfo, NULL, NULL);
     
@@ -185,6 +173,7 @@ int main()
         NET_DVR_Cleanup();
         return -1;
     }
+    printf("Init RealPlay SUCCESS\n");*/
     //Set callback function of getting stream.
     int iRet;
     //iRet = NET_DVR_SetRealDataCallBack(lRealPlayHandle, g_HikDataCallBack, 0);
@@ -199,20 +188,82 @@ int main()
         NET_DVR_Cleanup();  
         return -1;
     }
-#ifdef _WIN32
-    Sleep(5000);  //millisecond
-#elif  defined(__linux__)
     sleep(500);   //second
-#endif
+    return 0;
+}
+int main()
+{
+    NET_DVR_Init();
+    NET_DVR_SetExceptionCallBack_V30(0, NULL, g_ExceptionCallBack, NULL);
+    long lUserID;
+    char cUserChoose = 'r';
+    //login
+    NET_DVR_USER_LOGIN_INFO struLoginInfo = {0};
+    NET_DVR_DEVICEINFO_V40 struDeviceInfoV40 = {0};
+    struLoginInfo.bUseAsynLogin = false;
 
+
+    struLoginInfo.wPort = 8000;
+    memcpy(struLoginInfo.sDeviceAddress, "172.17.1.99", NET_DVR_DEV_ADDRESS_MAX_LEN);
+    memcpy(struLoginInfo.sUserName, "admin", NAME_LEN);
+    memcpy(struLoginInfo.sPassword, "Ab12345678", NAME_LEN);
+    lUserID = NET_DVR_Login_V40(&struLoginInfo, &struDeviceInfoV40);
+
+    if (lUserID < 0)
+    {
+        printf("pyd1---Login error, %d\n", NET_DVR_GetLastError());
+        return -1;
+    }
+    printf("Login SUCCESS\n");
+    //long lRealPlayHandle;
+    NET_DVR_PREVIEWINFO struPlayInfo = {0};
+    struPlayInfo.lChannel     = 1;  //channel NO
+    struPlayInfo.dwLinkMode   = 0;
+    struPlayInfo.bBlocked = 1;
+    struPlayInfo.dwDisplayBufNum = 1;
+    lRealPlayHandle = NET_DVR_RealPlay_V40(lUserID, &struPlayInfo, NULL, NULL);
+    
+    if (lRealPlayHandle < 0)
+    {
+        printf("pyd1---NET_DVR_RealPlay_V40 error\n");
+        NET_DVR_Logout(lUserID);
+        NET_DVR_Cleanup();
+        return -1;
+    }
+    printf("Init RealPlay SUCCESS\n");
+    //Demo_RealPlay(lUserID);
+    while ('q' != cUserChoose)
+    {
+        printf("\n");
+        printf("Input 1, Test RealPlay\n");
+        printf("      a, Test PTZControl LEFT\n");
+        printf("      p, Test PTZControl STOP\n");
+        printf("      q, Quit.\n");
+        
+        printf("Input:");
+
+        cin>>cUserChoose;
+        switch (cUserChoose)
+        {
+        case '1':
+            Demo_RealPlay(lUserID);
+            break;     
+        case 'a':
+            NET_DVR_PTZControlWithSpeed(lRealPlayHandle,PAN_LEFT,0,4); //Setting params.
+            break;
+        case 'p':
+            NET_DVR_PTZControlWithSpeed(lRealPlayHandle,PAN_LEFT,1,4); //Setting params.
+            break;            
+        default:
+            break; 
+        }          
+    
+    }
     //stop
     NET_DVR_StopRealPlay(lRealPlayHandle);
     NET_DVR_Logout_V30(lUserID);
     NET_DVR_Cleanup();
+    
     return 0;
-
-
-
-
 
 }
